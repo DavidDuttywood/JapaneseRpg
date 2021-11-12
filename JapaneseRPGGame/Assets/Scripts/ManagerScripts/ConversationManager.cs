005e0ff637afd7e3356b0bb9217215701349b5a5
David Woodward <david.owib@gmail.com>
2021-11-12 21:51:02 +0000

    more work on the conversation script.
    can move between questions and replies.
    tasty.

--- JapaneseRPGGame/Assets/Scripts/ManagerScripts/ConversationManager.cs
+++ JapaneseRPGGame/Assets/Scripts/ManagerScripts/ConversationManager.cs
@@ -7,21 +7,59 @@ using Data;
 public class ConversationManager : MonoBehaviour
 {
     public UITextTypeWriter npcText;
-    private int currentLine;
+    private int currentDialogueItem;
     private Conversation conversation;
     public GameObject menu;
+    public GameObject replyOptions;
 
-    // Start is called before the first frame update
+    private Button[] replyButtons;
+
+
     void Start()
     {
         conversation = new Conversation();
         conversation = conversation.GenerateTestConversation(); //this is shit
-        npcText.Type(conversation.ConversationItem[currentLine].NpcText); //this could be expanded to multi lines with subroutine
+        replyButtons = replyOptions.GetComponentsInChildren<Button>();
+
+        npcText.Type(conversation.ConversationItem[currentDialogueItem].NpcText); //this could be expanded to multi lines with subroutine
+        MapQuestionsToButtons(currentDialogueItem);
+    }
+
+    public void MapQuestionsToButtons(int currentDialogueItem)
+    {
+        int counter = 0;
+
+        foreach(Button b in replyButtons)
+        {
+            if (b.name.Contains("Option"))
+            {
+                Text replyText = b.GetComponentInChildren<Text>();
+                replyText.text = conversation.ConversationItem[currentDialogueItem].Replies[counter];
+                b.onClick.AddListener(delegate { ChooseReply(replyText.text); });
+
+            }
+            counter++;
+        }
+    }
+
+    public void ChooseReply(string reply)
+    {
+        if(reply == conversation.ConversationItem[currentDialogueItem].CorrectReply)
+        {
+            currentDialogueItem++;
+            npcText.Type(conversation.ConversationItem[currentDialogueItem].NpcText);
+            MapQuestionsToButtons(currentDialogueItem);
+        }
+        else
+        {
+            Debug.Log("No, thats not it");
+        }
+        return;
     }
 
     public void RepeatText()
     {
-        npcText.Type(conversation.ConversationItem[currentLine].NpcText);
+        npcText.Type(conversation.ConversationItem[currentDialogueItem].NpcText);
     }
 
     public void ShowHelpText()
@@ -32,11 +70,14 @@ public class ConversationManager : MonoBehaviour
     public void OpenReplyMenu()
     {
         menu.SetActive(false);
-
+        replyOptions.SetActive(true);
     }
 
-    public void OpenMainMenu()
+    public void CloseReplyMenuOpenMainMenu()
     {
+        replyOptions.SetActive(false);
         menu.SetActive(true);
     }
+
+
 }
