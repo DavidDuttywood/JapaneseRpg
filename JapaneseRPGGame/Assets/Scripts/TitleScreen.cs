8030535fdf5eb0aaeee479b4dce149859b142a55
David Woodward <david.owib@gmail.com>
2021-11-24 13:08:17 +0000

    title screen

--- JapaneseRPGGame/Assets/Scripts/TitleScreen.cs
+++ JapaneseRPGGame/Assets/Scripts/TitleScreen.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class TitleScreen : MonoBehaviour
 {
+    public Animator animator;
     public SceneTransitionManager stm;
     public void NewGame()
     {
         GameManager.instance.ClearSaveLogs();
-        stm.LoadLevel("BaseMechanicsSandbox");
+        LoadLevel("BaseMechanicsSandbox");
     }
 
     public void LoadGame()
     {
         //need a new save value in GameManager to remember which scene
+        stm.LoadLevel("BaseMechanicsSandbox");
+    }
+
+    public void LoadLevel(string targetScene)
+    {
+        StartCoroutine(StartGame(targetScene));
+    }
+
+    IEnumerator StartGame(string targetScene)
+    {
+        animator.SetTrigger("buttonPressed");
+        yield return new WaitForSeconds(3f);
         stm.LoadLevel("BaseMechanicsSandbox");
     }
 }
